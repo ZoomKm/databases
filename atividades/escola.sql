120635b9c33ad8a4ea1be0bb5335bf112979c685
HLR <150961832+ZoomKm@users.noreply.github.com>
2025-11-12 20:02:50 -0300

    escola

---
+++ atividades/escola.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE escola;
+
+USE escola;
+create table alunos(
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(70) NOT NULL,
+    idade int,
+    endereco varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
